66e86930632b80a2ca20abe35c31a702355f66a4
Michal <kubiakmichal@ymail.com>
2017-09-21 19:19:00 +0200

    Implemented DB storage

--- database_setup.py
+++ database_setup.py
@@ -1,19 +1,21 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy import DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class Flat(Base):
     __tablename__ = 'Flat'
     id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
+    name = Column(String(80))
     price = Column(String(8))
     link = Column(String(250))
+    location = Column(String(250))
     creation_date = Column(DateTime)
+    is_new = Column(Boolean)
 
 engine  = create_engine('sqlite:///flats.db')
 
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Flat
 
-class Flat:
-    pass
 
+engine = create_engine('sqlite:///flats.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 url = 'https://www.olx.pl/nieruchomosci/mieszkania/wynajem/gdansk/?search%5Bfilter_float_price%3Afrom%5D=600&search%5Bfilter_float_price%3Ato%5D=1200&search%5Bdist%5D=10'
 
@@ -12,25 +18,33 @@ r = requests.get(url)
 page = r.text
 
 soup = BeautifulSoup(page, "html.parser")
-
 ads = soup.find_all("table", summary="Ogłoszenie")
 
-flats = []
-
 for ad in ads:
-    flat = Flat()
-    flat.name = ad.find("td", valign="top")\
+
+
+    name = ad.find("td", valign="top")\
         .find("strong")\
         .get_text()\
         .strip()
-    flat.link = ad.find("a").get('href')
-    flat.price = ad.find("p", class_="price").get_text().strip()
-    flat.location = ad.find("td", valign="bottom")\
-        .find("small")\
-        .get_text()\
-        .strip()
 
-    flats.append(flat)
+    flat = session.query(Flat).filter_by(name = name).one_or_none()
+
+    if not flat:
+        flat = Flat()
+        flat.name = name
+        flat.link = ad.find("a").get('href')
+        flat.price = ad.find("p", class_="price").get_text().strip()
+        flat.location = ad.find("td", valign="bottom") \
+            .find("small") \
+            .get_text() \
+            .strip()
+
+        session.add(flat)
+        session.commit()
+
+
+flats = session.query(Flat).all()
 
 for flat in flats:
     print()
@@ -38,4 +52,3 @@ for flat in flats:
     print("link:", flat.link)
     print("cena:", flat.price)
     print("lokalizacja:", flat.location)
-
